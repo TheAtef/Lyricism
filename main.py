a5d851f8b196ecb59afd2db502ce56e1407f8fe8
TheAtef <69214639+TheAtef@users.noreply.github.com>
2023-10-10 18:12:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,11 +63,11 @@ class MyDeezer:
     def option(self, num):
         track = self.deezer.get_track(song.ids[num])
         name = track['info']['DATA']['SNG_TITLE'] 
-        track["download"](download_dir ='Songs\\temp', quality=track_formats.MP3_320, )
+        track["download"](download_dir ='Songs', quality=track_formats.MP3_320, )
         bot.send_chat_action(dl.chat.id, action='upload_audio')
-        bot.send_audio(dl.chat.id, open('Songs\\temp\\'+ name + '.mp3', 'rb'))
+        bot.send_audio(dl.chat.id, open('Songs\\'+ name + '.mp3', 'rb'))
         try:
-            bot.send_document(dl.chat.id, open('Songs\\temp\\' + name +'.lrc', 'rb'))
+            bot.send_document(dl.chat.id, open('Songs\\' + name +'.lrc', 'rb'))
         except:
             pass
 
